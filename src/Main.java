2ffd514761c69f1f8aae9cd4bf219d52ce656f6f
Ualink <kenshinwow1@gmail.com>
2023-08-24 17:31:38 +0300

    two-dimensional array

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 //massive
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[4];
-        Scanner scanner = new Scanner(System.in);
+    char[][] syms = new char[2][2];
+    syms[0][0] = 't';
+        System.out.println(syms[0][0]);
+        byte[][] nums = new byte[][]{
 
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print("int number: ");
-            int valvue = scanner.nextInt();
-            arr[i] = valvue;
-        }
-        int min = arr[0];
-        for (int i = 0;i < arr.length; i++){
-            if(arr[i] < min)
-                min = arr[i];
 
-        }
-        System.out.print("Minimal: " + min);
+                {5, 7},
+                {7, 3},
+                {2, 0}
+        };
+        nums[1][1] = 67;
+        System.out.println(nums[1][1]);
+
     }
 }
